3e88a15962d1cbbf3aecd7b7f8689aa9972fff1b
kundan kumar sahu <kundanlal96580@gmail.com>
2023-08-28 20:33:10 +0530

    Mui is not working

---
+++ crud/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import "./component/AddUser"
+import Adduser from './component/AddUser';
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+   <div className='App'>
+
+   <Navbar />
+   {/* <Adduser />
+    <h1>Hello from the app</h1> */}
+   </div>
+  );
+}
+
+export default App;
---
+++ crud/src/component/AddUser.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Adduser = () => {
+    <>
+        <h1>Hello from AddUser</h1>
+    </>
+};
+
+export default Adduser;

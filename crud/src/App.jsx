68710f71e33f1066e5ce4683bd5d997d847eaf91
kundan kumar sahu <kundanlal96580@gmail.com>
2023-08-29 20:02:49 +0530

    modified folder

--- crud/src/App.jsx
+++ crud/src/App.jsx
@@ -1,16 +1,23 @@
 import './App.css';
-import "./component/AddUser"
-import Adduser from './component/AddUser';
-import Navbar from './component/Navbar';
 
 function App() {
   return (
-   <div className='App'>
-
-   <Navbar />
-   {/* <Adduser />
-    <h1>Hello from the app</h1> */}
-   </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- crud/src/component/AddUser.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Adduser = () => {
-    <>
-        <h1>Hello from AddUser</h1>
-    </>
-};
-
-export default Adduser;
